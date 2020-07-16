e678194561d99ea2b2d43ef7c790f9b5d6dc09d3
halka <halkax84@gmail.com>
2020-07-16 18:10:31 +0000

    2020/7/16 fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,20 +7,14 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
 # prompt
 function fish_prompt
 
+    # ssh
     test $SSH_TTY
     and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
 
+    # Current directory
     printf (set_color cyan)(prompt_pwd)(set_color normal)
 
     # Git
