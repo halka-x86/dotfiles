13c2149a09d3637c8c388c8e9f7581957a85226f
halka <halkax84@gmail.com>
2024-06-08 09:45:46 +0900

    develop

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ function download_dotfiles() {
     git clone --recursive "${REMOTE_URL}" "${DOTFILES_DIRECTORY}"
   else
     # curlでダウンロード
-    curl -fsSLo --insecure ${HOME}/dotfiles.tar.gz ${DOTFILES_TARBALL}
+    curl -kfsSLo ${HOME}/dotfiles.tar.gz ${DOTFILES_TARBALL}
     tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOTFILES_DIRECTORY}
     rm -f ${HOME}/dotfiles.tar.gz
   fi
